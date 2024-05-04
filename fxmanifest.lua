4c84430b996e9865e08c26fd72ed65fcd97dd21f
Haxalott's Developments <43390732+Haxalott@users.noreply.github.com>
2024-05-04 19:10:24 +1000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game       'gta5'
+lua54      'yes'
+
+author      'Haxalott'
+description 'Rob atms'
+version     '1.0.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
